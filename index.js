81f03e05cfcefe46b4dd7ffeb5dc2a87c9d038e9
Umar falalu <marinijibiaa@gmail.com>
2024-09-03 18:51:44 -0700

    Pushing the code

---
+++ index.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+import { parse } from "csv-parse";
+
+const habitualPlanet = [];
+
+const isHabitualPlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.38 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitualPlanet(data)) {
+      habitualPlanet.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(
+      habitualPlanet.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitualPlanet.length} Habitual plants found`);
+  });
