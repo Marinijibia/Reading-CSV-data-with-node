a51820485ca263dffc0546c45a4383bf57d01b63
Umar falalu <marinijibiaa@gmail.com>
2024-09-05 20:05:07 -0700

    Testing express middleware

--- express_server/server.js
+++ express_server/server.js
@@ -22,6 +22,16 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const startTime = Date.now();
+  console.log(`${req.method} ${req.url}`);
+  next();
+  const delta = Date.now() - startTime;
+  console.log(`${delta} ms`);
+});
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to FBC");
 });
@@ -41,6 +51,23 @@ app.get("/friends/:friendId", (req, res) => {
   }
 
   res.status(200).send(friend);
+});
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({
+      error: "Kindly provide a name",
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
 });
 
 app.listen(PORT, () => {
